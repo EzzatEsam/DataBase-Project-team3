378500c070a84e8595d0f35bd3d819a09e339f24
Youssef Elshabrawy <YIElshabrawy@gmail.com>
2021-05-09 00:19:26 +0200

    Update Fighter.cpp

--- Enemies/Fighter.cpp
+++ Enemies/Fighter.cpp
@@ -1,12 +1,22 @@
 #include "Fighter.h"
 
-Fighter::Fighter(int id, int arrive, int hp, int atk, int relode, int ms)
+Fighter::Fighter(int id, int arrTime, double health, int spd, int firePower, int reloadPerioud, double d) : Enemy(id, arrTime, health, spd, firePower, reloadPerioud, d)
 {
-	id = id; ArrvTime = arrive; Health = hp; RelodeTime = relode; MovementSpeed = ms;
+}
+
+Fighter::~Fighter()
+{
 }
 
 void Fighter::Move()
 {
+    // If the health is half then the speed is halved as well
+    int moveSpd = Speed;
+    if (Health < MaxHealth / 2)
+        moveSpd /= 2;
+
+    // Move according to such speed
+    decDistanceByFactor(moveSpd);
 }
 
 void Fighter::Act()
