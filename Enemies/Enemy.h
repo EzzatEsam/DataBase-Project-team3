1c93dfc23a35b570b1e515a1176be8b142b11f15
Youssef Elshabrawy <YIElshabrawy@gmail.com>
2021-05-09 00:17:28 +0200

    Update Enemy.h

--- Enemies/Enemy.h
+++ Enemies/Enemy.h
@@ -10,46 +10,46 @@ class Enemy
 {
 
 protected:
-	int ID;         //Each enemy has a unique ID (sequence number)
-	int ArrvTime;	//arrival time (time it starts to join battle)
+	const int ID;		//Each enemy has a unique ID (sequence number)
+	const int ArrvTime; //arrival time (time it starts to join battle)
 
-	ENMY_STATUS status;	    //status of the enemy (inactive, inactive, frosted, killed)
-	int Distance;	//Horizontal distance between enemy & the tower of its region
-	                //Always positive (ranges from 2 to 60)
-	double Health;	//Enemy health
-	int RelodeTime;
-	int MovementSpeed;
-	
-	//
+	ENMY_STATUS status; //status of the enemy (inactive, inactive, frosted, killed)
+	double Distance;	//Horizontal distance between enemy & the tower of its region
+						//Always positive (ranges from 2 to 60)
+	double Health;		//Enemy health
+	double MaxHealth;
+
 	// TODO: Add More Data Members As Needed
-	//
+	int Speed;
+	int FirePower;
+	int ReloadPeriod;
 
 public:
-
-	Enemy() {};
-	Enemy(int id, int arrTime, int d = MaxDistance);
+	Enemy(int id, int arrTime, double d = MaxDistance);
+	Enemy(int id, int arrTime, double health, int spd, int firePower, int reloadPerioud, double d = MaxDistance);
 	virtual ~Enemy();
 
 	int Enemy::GetID() const;
 	ENMY_STATUS GetStatus() const;
 	void SetStatus(ENMY_STATUS);
-	
+
 	void DecrementDist();
 
-	void SetDistance(int );
+	void SetDistance(int);
 	int GetDistance() const;
 
 	int GetArrvTime() const;
 
-
 	// Virtual Functions: ----------------
 
-	virtual void Move() = 0;	//All enemies can move
-	virtual void Act() = 0;	//Acting means fighting or healing
+	virtual void Move() = 0; //All enemies can move
+	virtual void Act() = 0;	 //Acting means fighting or healing
 
 	//
 	// TODO: Add More Member Functions As Needed
-	//
+	void decDistanceByFactor(double factor);
 
+	void setHealth(double val);
+	double getHealth();
+	void increaseHealth(double val);
 };
-
