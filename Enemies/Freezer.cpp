fda0e4f6ab6e416d556b027582adbf6e599d9534
Youssef Elshabrawy <YIElshabrawy@gmail.com>
2021-05-09 00:16:49 +0200

    Update Freezer.cpp

--- Enemies/Freezer.cpp
+++ Enemies/Freezer.cpp
@@ -1,12 +1,22 @@
 #include "Freezer.h"
 
-Freezer::Freezer(int id, int arrive, int hp, int atk, int relode, int ms)
+Freezer::Freezer(int id, int arrTime, double health, int spd, int firePower, int reloadPerioud, double d) : Enemy(id, arrTime, health, spd, firePower, reloadPerioud, d)
 {
-	id = id; ArrvTime = arrive; Health = hp; RelodeTime = relode; MovementSpeed = ms;
+}
+
+Freezer::~Freezer()
+{
 }
 
 void Freezer::Move()
 {
+    // If the health is half then the speed is halved as well
+    int moveSpd = Speed;
+    if (Health < MaxHealth / 2)
+        moveSpd /= 2;
+
+    // Move according to such speed
+    decDistanceByFactor(moveSpd);
 }
 
 void Freezer::Act()
