4ad0992590aebaa2bd57852c3b8a4ce15d7ede98
Youssef Elshabrawy <YIElshabrawy@gmail.com>
2021-05-09 00:30:45 +0200

    Update Castle.cpp

--- Castle/Castle.cpp
+++ Castle/Castle.cpp
@@ -1,7 +1,5 @@
 #include "Castle.h"
-
-
-
+#include "../Enemies/Healer.h"
 
 void Castle::SetAtkNum(int n)
 {
@@ -15,7 +13,7 @@ void Castle::SetAtkDamage(int d)
 
 void Castle::SetHealth(double h)
 {
-	if(h > 0)
+	if (h > 0)
 		Health = h;
 	else
 		Health = 0; // killed
@@ -34,4 +32,22 @@ int Castle::GetAtkDamage() const
 int Castle::GetAtkNum() const
 {
 	return AtkNum;
+}
+
+void Castle::Fire(Enemy *pE)
+{
+	int k = 1;
+	Healer *pHealer = dynamic_cast<Healer *>(pE);
+	if (pHealer != nullptr)
+		k = 2;
+	else
+		k = 1;
+	double dmg = (1.0 / pE->GetDistance()) * firePower * 1.0 / k;
+
+	pE->increaseHealth(-dmg);
+}
+
+void Castle::Freeze(Enemy *pE)
+{
+	pE->SetStatus(FRST);
 }
