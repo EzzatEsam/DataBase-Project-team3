4da0f5f06678eb8d669f4369d427a83b607e0f22
Ezzat Esam <s-ezzat.eisawy@zewailcity.edu.eg>
2021-06-15 02:37:40 +0200

    sdsds

--- Demo_Main.cpp
+++ Demo_Main.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 int main()
 {
-	/*Battle* pGameBattle = new Battle;
+	Battle* pGameBattle = new Battle;
 	pGameBattle->RunSimulation();
 	delete pGameBattle;
-	return 0;*/
-	MaxHeap<int> test;
-	test.insert(5);
-	test.insert(4);
-	test.insert(10);
-	test.insert(3);
-	test.insert(6);
-	test.insert(8);
-	test.insert(7);
-	int x;
-	while (test.BeHead(x))
-	{
-		cout << x;
-	}
-	cout << "sds";
+	return 0;
+	//MaxHeap<int> test;
+	//test.insert(5);
+	//test.insert(4);
+	//test.insert(10);
+	//test.insert(3);
+	//test.insert(6);
+	//test.insert(8);
+	//test.insert(7);
+	//int x;
+	//while (test.BeHead(x))
+	//{
+	//	cout << x;
+	//}
+	//cout << "sds";
 }
